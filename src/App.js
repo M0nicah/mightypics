32a47548b615eb7c503012b17021fc64c1d855a5
Monica Masae <monicamasae@Peters-MacBook-Air.local>
2022-07-25 17:04:40 +0300

    add hero section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div>
+        <ul class="nav justify-content-center">
+          <li class="nav-item">
+            <a class="nav-link active link" aria-current="page" href="!#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link link" href="!#">Gallery</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link link" href="!#">Contacts</a>
+          </li>
+        </ul>
+      </div>
+      <section>
+        <div>
+          <h1 className='hero-text'>MightyPics Photography</h1>
+          <p className='text'>Your one stop shop for <span>quality photos!</span></p>
+          <button type='submit' className='button'>Book Now</button>
+        </div>
+        <div className='hero-image'>
+          <img src="" alt="hero" />
+        </div>
+
+      </section>
+
+
+    </body>
   );
 }
 
