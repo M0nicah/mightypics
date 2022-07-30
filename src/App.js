8a3b295fc21d026a5109ec6380e99cfa160f0a0d
Monica Masae <monicamasae@iMac.local>
2022-07-30 12:13:52 +0300

    add animation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import React from 'react';
 import './App.css';
 
 function App() {
+
   return (
     <body>
       <div>
@@ -19,19 +19,31 @@ function App() {
           </li>
         </ul>
       </div>
-      <section>
+      <main>
         <div>
           <h1 className='hero-text'>MightyPics Photography</h1>
           <p className='text'>Your one stop shop for <span>quality photos!</span></p>
           <button type='submit' className='button'>Book Now</button>
         </div>
-        <div className='hero-image'>
-          <img src="" alt="hero" />
-        </div>
-
-      </section>
-
-
+        <figure>
+          <div class="carousel dissolve">
+            <div class="items">
+              <img class="item" src={require("../src/images/grad1.JPG")} alt=""/>
+              <img class="item" src={require("../src/images/grad2.JPG")} alt="Seven to ten at Leeds &amp; Holbeck"/>
+              <img class="item" src={require("../src/images/grad3.JPG")} alt="Banksy"/>
+              <img class="item" src={require("../src/images/wed1.jpg")} alt="Bristol autumn"/>
+              <img class="item" src={require("../src/images/wed1.jpg")} alt="Bristol autumn"/>
+              <img class="item" src={require("../src/images/wed2.jpg")} alt="Bristol autumn"/>
+              <img class="item" src={require("../src/images/rand1.jpg")} alt="Bristol autumn"/>
+              <img class="item" src={require("../src/images/rand2.jpg")} alt="Bristol autumn"/>
+              <img class="item" src={require("../src/images/rand3.jpg")} alt="Bristol autumn"/>
+              <img class="item" src={require("../src/images/rand4.jpg")} alt="Bristol autumn"/>
+            </div>
+          </div>
+          <figcaption>High quality shots</figcaption>
+          <p>To get more quality photos and other services  <a className='info' href="!#">book with us!</a></p>
+        </figure>
+      </main>
     </body>
   );
 }
